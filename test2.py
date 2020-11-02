f0c627cf69e84b62b6c2e59c6588dffb66d12f56
Your Name <you@example.com>
2020-11-02 20:39:31 +0800

    第二次版本

--- test2.py
+++ test2.py
@@ -8,6 +8,10 @@ import scipy.interpolate as interpol
 from PyEMD import EMD,EEMD,CEEMDAN   #导入ENMD相关包
 fs=360
 record=wfdb.rdrecord('101',physical=False,channels=[0]).d_signal.flatten() #原数据为单独列表，并不是一个list，卡2天
+
+plt.rcParams['savefig.dpi'] = 300 #图片像素
+plt.rcParams['figure.dpi'] = 500 #分辨率
+
 f1=0.083
 f2=0.13
 f3=0.32
@@ -20,8 +24,6 @@ data2=signal.lfilter(b2,1,record)
 data3=signal.lfilter(b3,1,data2)
 # coffees=pywt.wavedec(data2,wavelet='db10',level=8)
 # print(coffees[7])
-
-
 plt.rcParams['savefig.dpi'] = 300 #图片像素
 plt.rcParams['figure.dpi'] = 500 #分辨率
 data3[0:100]=min(data3[200:600])
@@ -29,9 +31,9 @@ locmin=signal.argrelmin(data2,order=100)
 locmin=locmin[0]
 x=np.linspace(0,len(data2)-1)
 curve=interpol.interp1d(locmin,data2[locmin])
-a,d=pywt.dwt(data2,'db8','constant')
-plt.plot(d)
-plt.show()
+
+
+
 
 
 
