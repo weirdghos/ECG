2a25b26504269363a0c7136408d89750b45bf285
Your Name <you@example.com>
2020-11-10 15:55:27 +0800

    评价函数建好，使用MIT信号进行尝试滤波，已有小波，EMD，

--- nosie_perf.py
+++ nosie_perf.py
@@ -1,25 +1,22 @@
 
 import numpy as np
-
-
-
+from scipy import stats
 class sig():
   def __init__(self,original_signal,reconstructed_signal):
-      self.original_signal=     original_signal
-      self.reconstructed_signal=reconstructed_signal
+      self.original_signal=     original_signal          #b表示干净信号
+      self.reconstructed_signal=reconstructed_signal     #去噪后的信号
   def SNR(self):
      #滤波后的ECG信号/噪声 求分贝
-     snr=10*np.log10(np.sum(self.reconstructed_signal**2)/np.sum((self.reconstructed_signal-self.original_signal)**2))
+     snr=10*np.log10(np.sum(self.original_signal**2)/np.sum((self.reconstructed_signal-self.original_signal)**2))
      return snr
   def PRD(self):   #百分比均方根误差
-     prd=np.sqrt((self.reconstructed_signal-self.original_signal)**2 /self.reconstructed_signal**2)
+     prd=np.sqrt((self.reconstructed_signal-self.original_signal)**2 /self.original_signal)   #公式可能有问题 再找一哈
      return prd
-  def PRD1(self):
-     prd1=np.sqrt()
-     return prd1
-a=np.random.rand(10)
-b=np.random.rand(10)
-p1=sig(a,b)
-print(p1.SNR())
+  def MSE(self): #均方误差
+     mse=(np.sum((self.original_signal-self.reconstructed_signal)**2))/len(self.original_signal)
+     return mse
+  def corelete(self):  #皮尔森相关系数
+     pccs=stats.pearsonr(self.original_signal,self.reconstructed_signal)
+     return pccs
 
 
